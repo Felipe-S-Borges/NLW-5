eb2b64e9c2cf100b73f7968091dd1aa7d5f51d7f
Felipe <felipelee22@gmail.com>
2021-05-06 22:53:30 -0300

    feature: dark mode

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,7 @@
 import format from 'date-fns/format';
 import ptBR from 'date-fns/locale/pt-BR';
+import { useEffect, useRef, useState } from 'react';
+
 import styles from '../Header/styles.module.scss';
 
 
@@ -7,12 +9,26 @@ export default function Header(){
     const currentDate = format(new Date(), 'EEEEEE, d MMM',{
         locale: ptBR,
     })
+    
+    const [isDark, setIsDark] = useState(false)
+
+    function toggleDarkMode(){
+        setIsDark(!isDark)
+    }
+    useEffect(()=>{
+        console.log(isDark)
+        document.querySelector("html").setAttribute('data-dark',String(isDark))
+    },[isDark])
 
     return(    
             <header className={styles.headerContainer}>
                 <img src="/logo.svg" alt="Podcast"/>
                 <p> O melhor para você ouvir, sempre</p>
                 <span>{currentDate}</span>
+                <label className={styles.switch}>
+                    <input type="checkbox" onChange={toggleDarkMode}/>
+                    <span className={`${styles.slider} ${styles.round}`}></span>
+                </label>
             </header>
     )
 }
